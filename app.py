1cae45317c2ac853569d0cb34b8eb6ed387bd742
Luis Cesar Albuquerque Arruda <lc.albuquerque@edu.unifor.br>
2024-12-03 13:45:33 -0300

    Ajustes no codigo

--- app.py
+++ app.py
@@ -48,11 +48,19 @@ def tsp(grafo, inicio):
 
 # Dados do problema
 brinquedos = {
-    'A': {'tempo_espera': 10, 'distancias': {'B': 100, 'C': 150}},
-    'B': {'tempo_espera': 5, 'distancias': {'A': 100, 'C': 120, 'D': 200}},
-    'C': {'tempo_espera': 8, 'distancias': {'A': 150, 'B': 120, 'D': 90}},
-    'D': {'tempo_espera': 12, 'distancias': {'B': 200, 'C': 90}}
+    'Insano': {'tempo_espera': 20, 'distancias': {'Kalafrio': 100, 'Vaikuntudo': 150, 'Maremoto': 200, 'Arrepius': 180, 'Ramubrinká': 170, 'Tobomusik': 90, 'Atlantis': 120, 'Aqua Show': 180, 'Moréia Negra': 150, 'Ilha do Tesouro': 100}},
+    'Kalafrio': {'tempo_espera': 10, 'distancias': {'Insano': 100, 'Vaikuntudo': 120, 'Arrepius': 180, 'Ramubrinká': 170, 'Tobomusik': 110, 'Atlantis': 140, 'Aqua Show': 160, 'Moréia Negra': 180, 'Ilha do Tesouro': 130}},
+    'Vaikuntudo': {'tempo_espera': 15, 'distancias': {'Insano': 150, 'Kalafrio': 120, 'Maremoto': 90, 'Arrepius': 200, 'Ramubrinká': 150, 'Tobomusik': 100, 'Atlantis': 130, 'Aqua Show': 160, 'Moréia Negra': 140, 'Ilha do Tesouro': 180}},
+    'Maremoto': {'tempo_espera': 12, 'distancias': {'Insano': 200, 'Vaikuntudo': 90, 'Ramubrinká': 170, 'Ilha do Tesouro': 70, 'Arrepius': 120, 'Tobomusik': 130, 'Atlantis': 140, 'Aqua Show': 180, 'Moréia Negra': 160}},
+    'Arrepius': {'tempo_espera': 18, 'distancias': {'Kalafrio': 180, 'Insano': 180, 'Vaikuntudo': 200, 'Maremoto': 120, 'Ramubrinká': 150, 'Tobomusik': 180, 'Atlantis': 160, 'Aqua Show': 140, 'Moréia Negra': 100, 'Ilha do Tesouro': 190}},
+    'Ramubrinká': {'tempo_espera': 8, 'distancias': {'Maremoto': 170, 'Insano': 170, 'Vaikuntudo': 150, 'Arrepius': 150, 'Tobomusik': 100, 'Atlantis': 130, 'Aqua Show': 180, 'Moréia Negra': 110, 'Ilha do Tesouro': 160}},
+    'Tobomusik': {'tempo_espera': 10, 'distancias': {'Atlantis': 120, 'Aqua Show': 100, 'Insano': 90, 'Kalafrio': 110, 'Vaikuntudo': 100, 'Maremoto': 130, 'Arrepius': 180, 'Ramubrinká': 100, 'Ilha do Tesouro': 150}},
+    'Atlantis': {'tempo_espera': 7, 'distancias': {'Tobomusik': 120, 'Aqua Show': 150, 'Insano': 120, 'Kalafrio': 140, 'Vaikuntudo': 130, 'Maremoto': 140, 'Arrepius': 160, 'Ramubrinká': 130, 'Ilha do Tesouro': 110}},
+    'Aqua Show': {'tempo_espera': 30, 'distancias': {'Tobomusik': 100, 'Ilha do Tesouro': 200, 'Insano': 180, 'Kalafrio': 160, 'Vaikuntudo': 160, 'Maremoto': 180, 'Arrepius': 140, 'Ramubrinká': 180, 'Atlantis': 150}},
+    'Moréia Negra': {'tempo_espera': 14, 'distancias': {'Ilha do Tesouro': 160, 'Arrepius': 120, 'Insano': 150, 'Kalafrio': 180, 'Vaikuntudo': 140, 'Maremoto': 160, 'Tobomusik': 180, 'Ramubrinká': 110, 'Atlantis': 130}},
+    'Ilha do Tesouro': {'tempo_espera': 12, 'distancias': {'Maremoto': 70, 'Aqua Show': 200, 'Moréia Negra': 160, 'Insano': 100, 'Kalafrio': 130, 'Vaikuntudo': 180, 'Arrepius': 190, 'Ramubrinká': 160, 'Tobomusik': 150, 'Atlantis': 110}}
 }
+
 
 grafo = {}
 for brinquedo, dados in brinquedos.items():
@@ -64,13 +72,22 @@ for brinquedo, dados in brinquedos.items():
 
 # Coordenadas fictícias dos brinquedos para o gráfico
 coordenadas = {
-    "A": {"x": 1, "y": 5},
-    "B": {"x": 2, "y": 3},
-    "C": {"x": 4, "y": 8},
-    "D": {"x": 7, "y": 1}
+    "Insano": {"x": 1, "y": 5},
+    "Kalafrio": {"x": 2, "y": 3},
+    "Vaikuntudo": {"x": 4, "y": 8},
+    "Maremoto": {"x": 7, "y": 1},
+    "Arrepius": {"x": 3, "y": 7},
+    "Ramubrinká": {"x": 7, "y": 4},
+    "Tobomusik": {"x": 8, "y": 2},
+    "Atlantis": {"x": 9, "y": 6},
+    "Aqua Show": {"x": 12, "y": 7},
+    "Ilha do Tesouro": {"x": 11, "y": 2},
+    "Moréia Negra": {"x": 14, "y": 8}
 }
 
-inicio = 'A'
+
+
+inicio = 'Moréia Negra'
 rota, custo = tsp(grafo, inicio)
 
 # Rota API
@@ -83,4 +100,4 @@ def rota_api():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug= True,port=8080)
