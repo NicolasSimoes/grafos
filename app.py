4f8defad53ea0b67ff42b84026fb4f13b83afb9f
Luis Cesar Albuquerque Arruda <lc.albuquerque@edu.unifor.br>
2024-12-05 15:22:13 -0300

    atualizando

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, json, jsonify, request
 from itertools import permutations
 from flask_cors import CORS  # Importando o CORS
 
@@ -23,6 +23,19 @@ def calcular_custo(rota, grafo):
             custo_total += peso
             break
     return custo_total
+
+def calcular_custo_por_no(grafo):
+    custo_por_no = {}
+
+    for origem, destinos in grafo.items():
+        custo_total = 0
+        
+        for destino, peso in destinos:
+            custo_total += peso
+        
+        custo_por_no[origem] = custo_total
+
+    return custo_por_no
 
 def vizinho_mais_proximo(grafo, inicio):
     visitados = {inicio}
@@ -66,15 +79,21 @@ brinquedos = {
     'Ilha do Tesouro': {'tempo_espera': 12, 'distancias': {'Maremoto': 70, 'Aqua Show': 200, 'Moréia Negra': 160, 'Insano': 100, 'Kalafrio': 130, 'Vaikuntudo': 180, 'Arrepius': 190, 'Ramubrinká': 160, 'Tobomusik': 150, 'Atlantis': 110}}
 }
 
+# Criando o grafo inicial
+def criar_grafo_completo(brinquedos):
+    novo_grafo = {}
+    for brinquedo, dados in brinquedos.items():
+        novo_grafo[brinquedo] = []
+        for destino, distancia in dados['distancias'].items():
+            tempo_espera = brinquedos[destino]['tempo_espera']
+            peso = calcular_peso(distancia, tempo_espera)
+            novo_grafo[brinquedo].append((destino, peso))
+    return novo_grafo
 
-grafo = {}
-for brinquedo, dados in brinquedos.items():
-    grafo[brinquedo] = []
-    for destino, distancia in dados['distancias'].items():
-        tempo_espera = brinquedos[destino]['tempo_espera']
-        peso = calcular_peso(distancia, tempo_espera)
-        grafo[brinquedo].append((destino, peso))
+# Inicializando o grafo
+grafo = criar_grafo_completo(brinquedos)
 
+# Dados das coordenadas
 coordenadas = {
     "Insano": {"x": 1, "y": 5},
     "Kalafrio": {"x": 2, "y": 3},
@@ -89,122 +108,30 @@ coordenadas = {
     "Moréia Negra": {"x": 14, "y": 8}
 }
 
+# Inicializando o ponto de partida e rota
 inicio = 'Insano'
-rota, custo = rota, custo = vizinho_mais_proximo(grafo, inicio)
-@app.route('/atualizar-inicio', methods=['POST'])
-def atualizar_inicio():
-    global inicio
+rota, custo = vizinho_mais_proximo(grafo, inicio)
 
-    try:
-        dados = request.get_json()
-        novo_inicio = dados.get('inicio')
-
-        # Verifica se o nó existe no grafo
-        if novo_inicio and novo_inicio in grafo:
-            inicio = novo_inicio
-            return jsonify({'mensagem': f'Nó inicial atualizado para {inicio}'}), 200
-        else:
-            return jsonify({'mensagem': 'Nó inválido'}), 400
-    except Exception as e:
-        return jsonify({'mensagem': str(e)}), 500
 # Rotas da API
 @app.route('/rota', methods=['GET'])
 def rota_api():
+    custo_por_no = calcular_custo_por_no(grafo)
     return jsonify({
         "rota": rota,
         "custo": custo,
         "grafo": coordenadas,
-        "inicio": inicio
+        "inicio": inicio,
+        "custo_por_no": custo_por_no
     })
 
-@app.route('/atualizar-tempo', methods=['POST', 'OPTIONS'])
-def atualizar_tempo_espera():
-    if request.method == 'OPTIONS':
-        return '', 200  # Responde ao preflight
-    
-    dados = request.get_json()
-    brinquedo = dados.get('brinquedo')
-    novo_tempo = dados.get('tempo')
-    
-    if brinquedo in brinquedos:
-        # Atualizar o tempo de espera
-        brinquedos[brinquedo]['tempo_espera'] = novo_tempo
-        
-        # Recriar o grafo completo
-        global grafo
-        grafo = criar_grafo_completo(brinquedos)
-        
-        # Recalcular a rota
-        global rota, custo
-        rota, custo = rota, custo = vizinho_mais_proximo(grafo, inicio)
-        
-        return jsonify({
-            "mensagem": "Tempo atualizado e grafo recalculado com sucesso!",
-            "rota": rota,
-            "custo": custo
-        })
-    return jsonify({"erro": "Brinquedo não encontrado"}), 400
-
-
-def criar_grafo_completo(brinquedos):
-    novo_grafo = {}
-    for brinquedo, dados in brinquedos.items():
-        novo_grafo[brinquedo] = []
-        for destino, distancia in dados['distancias'].items():
-            tempo_espera = brinquedos[destino]['tempo_espera']
-            peso = calcular_peso(distancia, tempo_espera)
-            novo_grafo[brinquedo].append((destino, peso))
-    return novo_grafo
-
-
-@app.route('/reset', methods=['POST'])
-def resetar_dados():
-    global brinquedos, grafo, rota, custo
-
-    # Dados iniciais dos brinquedos
-    brinquedos_iniciais = {
-        'Insano': {'tempo_espera': 20, 'distancias': {'Kalafrio': 100, 'Vaikuntudo': 150, 'Maremoto': 200, 'Arrepius': 180, 'Ramubrinká': 170, 'Tobomusik': 90, 'Atlantis': 120, 'Aqua Show': 180, 'Moréia Negra': 150, 'Ilha do Tesouro': 100}},
-        'Kalafrio': {'tempo_espera': 10, 'distancias': {'Insano': 100, 'Vaikuntudo': 120, 'Arrepius': 180, 'Ramubrinká': 170, 'Tobomusik': 110, 'Atlantis': 140, 'Aqua Show': 160, 'Moréia Negra': 180, 'Ilha do Tesouro': 130}},
-        'Vaikuntudo': {'tempo_espera': 15, 'distancias': {'Insano': 150, 'Kalafrio': 120, 'Maremoto': 90, 'Arrepius': 200, 'Ramubrinká': 150, 'Tobomusik': 100, 'Atlantis': 130, 'Aqua Show': 160, 'Moréia Negra': 140, 'Ilha do Tesouro': 180}},
-        'Maremoto': {'tempo_espera': 12, 'distancias': {'Insano': 200, 'Vaikuntudo': 90, 'Ramubrinká': 170, 'Ilha do Tesouro': 70, 'Arrepius': 120, 'Tobomusik': 130, 'Atlantis': 140, 'Aqua Show': 180, 'Moréia Negra': 160}},
-        'Arrepius': {'tempo_espera': 18, 'distancias': {'Kalafrio': 180, 'Insano': 180, 'Vaikuntudo': 200, 'Maremoto': 120, 'Ramubrinká': 150, 'Tobomusik': 180, 'Atlantis': 160, 'Aqua Show': 140, 'Moréia Negra': 100, 'Ilha do Tesouro': 190}},
-        'Ramubrinká': {'tempo_espera': 8, 'distancias': {'Maremoto': 170, 'Insano': 170, 'Vaikuntudo': 150, 'Arrepius': 150, 'Tobomusik': 100, 'Atlantis': 130, 'Aqua Show': 180, 'Moréia Negra': 110, 'Ilha do Tesouro': 160}},
-        'Tobomusik': {'tempo_espera': 10, 'distancias': {'Atlantis': 120, 'Aqua Show': 100, 'Insano': 90, 'Kalafrio': 110, 'Vaikuntudo': 100, 'Maremoto': 130, 'Arrepius': 180, 'Ramubrinká': 100, 'Ilha do Tesouro': 150}},
-        'Atlantis': {'tempo_espera': 7, 'distancias': {'Tobomusik': 120, 'Aqua Show': 150, 'Insano': 120, 'Kalafrio': 140, 'Vaikuntudo': 130, 'Maremoto': 140, 'Arrepius': 160, 'Ramubrinká': 130, 'Ilha do Tesouro': 110}},
-        'Aqua Show': {'tempo_espera': 30, 'distancias': {'Tobomusik': 100, 'Ilha do Tesouro': 200, 'Insano': 180, 'Kalafrio': 160, 'Vaikuntudo': 160, 'Maremoto': 180, 'Arrepius': 140, 'Ramubrinká': 180, 'Atlantis': 150}},
-        'Moréia Negra': {'tempo_espera': 14, 'distancias': {'Ilha do Tesouro': 160, 'Arrepius': 120, 'Insano': 150, 'Kalafrio': 180, 'Vaikuntudo': 140, 'Maremoto': 160, 'Tobomusik': 180, 'Ramubrinká': 110, 'Atlantis': 130}},
-        'Ilha do Tesouro': {'tempo_espera': 12, 'distancias': {'Maremoto': 70, 'Aqua Show': 200, 'Moréia Negra': 160, 'Insano': 100, 'Kalafrio': 130, 'Vaikuntudo': 180, 'Arrepius': 190, 'Ramubrinká': 160, 'Tobomusik': 150, 'Atlantis': 110}}
-    }
-
-    # Dados de coordenadas
-    coordenadas_iniciais = {
-        "Insano": {"x": 1, "y": 5},
-        "Kalafrio": {"x": 2, "y": 3},
-        "Vaikuntudo": {"x": 4, "y": 8},
-        "Maremoto": {"x": 7, "y": 1},
-        "Arrepius": {"x": 3, "y": 7},
-        "Ramubrinká": {"x": 7, "y": 4},
-        "Tobomusik": {"x": 8, "y": 2},
-        "Atlantis": {"x": 9, "y": 6},
-        "Aqua Show": {"x": 12, "y": 7},
-        "Ilha do Tesouro": {"x": 11, "y": 2},
-        "Moréia Negra": {"x": 14, "y": 8}
-    }
-
-
-    brinquedos = brinquedos_iniciais.copy()
-    coordenadas = coordenadas_iniciais.copy()
-
- 
+@app.route('/atualizar', methods=['POST'])
+def atualizar_api():
+    global brinquedos, grafo, coordenadas
+    dados = request.json
+    brinquedos = dados.get('brinquedos', brinquedos)
     grafo = criar_grafo_completo(brinquedos)
-
-  
-    global rota, custo
-    rota, custo = rota, custo = vizinho_mais_proximo(grafo, inicio)
-
-    return jsonify({"mensagem": "Dados resetados com sucesso!", "rota": rota, "custo": custo})
-
-
+    coordenadas = dados.get('coordenadas', coordenadas)
+    return jsonify({"message": "Dados atualizados com sucesso"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port= 8080)
